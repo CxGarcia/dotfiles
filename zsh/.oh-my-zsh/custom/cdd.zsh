946160b60d1aed0c82f3845f16ba81c1183b12c5
cxgarcia <schlaubitzcristobal@gmail.com>
2023-06-21 17:16:04 +0200

    sync commands and extensions

--- zsh/.oh-my-zsh/custom/cdd.zsh
+++ zsh/.oh-my-zsh/custom/cdd.zsh
@@ -1,8 +1,20 @@
 cdd() {
-  if [ "$1" != "" ]
+  if [ -z "$1" ]
     then
-      cd "$DEV/$1"
+      cd "$DEV"
     else
-  cd "$DEV"
+      cd "$DEV/$1"
   fi
+}
+
+seekndestroy() {
+  if [ -z "$1" ]
+    then
+      echo "Please provide a file name or directory to delete"
+      return
+  fi
+
+  find . -name "$1" -type d -prune -exec rm -rf '{}' +
+
+  echo "Obliberated all '$1' directories"
 }
